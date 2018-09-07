d6d8f669540dd7e00b85880576ab37ff9028864d
Thrisnadevany <thrisnamalia@gmail.com>
2018-09-07 22:22:35 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,26 @@
+public class Mahasiswa {
+	String nama;
+	String npm;
+
+	public Mahasiswa(String nama, String npm) {
+		super();
+		this.nama = nama;
+		this.npm = npm;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String namaBaru) {
+		this.nama = namaBaru;
+	}
+
+	public String getNpm() {
+		return npm;
+	}
+
+	public void setNpm(String npmBaru) {
+		this.npm = npmBaru;
+	}
+}
